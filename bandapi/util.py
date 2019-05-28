8e10339a00da447d4dbd6167543bb62452ac6faa
Jinho Hwang <howden2@gmail.com>
2019-05-28 13:56:10 -0400

    Cleaned up utils

--- bandapi/util.py
+++ bandapi/util.py
@@ -1,46 +1,16 @@
+"""
+Utility function
+"""
+
 from requests import request
 
 from bandapi import config
+import json
 
-
-def do_request(url, method='GET'):
+def print_json(string, indent=4, sort_keys=False):
     """
-    Makes a request and returns the result.
+    Neatly print string into json-like format.
     """
-    res = request(method=method, url=url)
-    return res
-
-
-def make_url(base, kwargs={}):
-    """
-    base -- str
-        The base url until argument.
-    kwargs -- dict
-        The key arguments dict to be placed in url
-
-    base + kwargs
-    -->
-    "baseurl?key1=value1&key2=value2&...&keyN=valueN"
-    """
-    kwargs = {'access_token': config.ACCESS_TOKEN, **kwargs}
-    params = []
-
-    # [key=value, key=value, ... ]
-    for key, value in kwargs.items():
-        if value:
-            params.append(f'{key}={value}')
-    # 'key=value&key=value...'
-    params_url = '&'.join(params)
-    # 'base?key=value&key=value...'
-    url = f'{base}?{params_url}'
-
-    return url
-
-
-def api_request(base, kwargs={}, method='GET'):
-    """
-    Does api request and gets the results back.
-    """
-    url = make_url(base, kwargs)
-    res = do_request(url, method=method)
-    return res
+    obj = json.loads(string)
+    string = json.dumps(obj, indent=indent, sort_keys=sort_keys)
+    print(string)
