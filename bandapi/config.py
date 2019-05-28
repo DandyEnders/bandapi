baf7ff2f7ab7eb27806c23776ba3b812597bbdd2
Jinho Hwang <howden2@gmail.com>
2019-05-28 13:55:54 -0400

    Added neccessary configs

--- bandapi/config.py
+++ bandapi/config.py
@@ -1,1 +1,7 @@
-ACCESS_TOKEN = ""
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+# this is from the web profile but
+# I have no idea where this is used for.
+# This does not work as access_token on actual api call.
+_ACCESS_TOKEN = ""
+CLIENT_REDIRECT_URL = ""
